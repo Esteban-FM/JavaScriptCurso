4682d1ecefca0fdcc5fcbaccb1c26537f6eb613d
Esteban FM <esteban.fsma@gmail.com>
2024-06-07 14:55:56 -0600

    changes and add operators

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,22 @@
+
+//Declaraciones
+let a = 5;
+let b = 7;
+
+//espresion
+console.log( a + b, 'adición' );             //adición
+console.log( a - b, 'resta' );               //resta
+console.log( a * b, 'multiplicación' );      //multiplicación
+console.log( a / b, 'division' );            //división
+console.log( a % b, 'módulo' );              //módulo
+console.log( a ** b, 'potencia' );           //potencia
+
+//incrementar
+console.log(++a); //incrementa el valor de "a" en 1
+console.log(a++); //imprime el valor de "a", luego incrementa su valor en 1 sin mostrarlo
+console.log(a);   //imprime el valor de "a" con el valor final despues de los operadores anteriores
+
+//decrementar
+console.log(--a); //decrementa el valor de "a" en 1
+console.log(a--); //imprime el valor de "a", luego decrementa su valor en 1 sin mostrarlo
+console.log(a);   //imprime el valor de "a" con el valor final despues de los operadores anteriores
